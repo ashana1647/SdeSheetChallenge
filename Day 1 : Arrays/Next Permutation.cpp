7fe74eafc0bed05499d16aa6b835c85c041ab0b9
Ashana <60913775+ashana1647@users.noreply.github.com>
2022-06-08 00:30:52 +0530

    Update Next Permutation.cpp

--- Day 1 : Arrays/Next Permutation.cpp
+++ Day 1 : Arrays/Next Permutation.cpp
@@ -1,1 +1,24 @@
-
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+    int l,k;
+    for( k=n-2;k>=0;k--)
+    {
+        if(permutation[k]<permutation[k+1])
+            break;
+    }
+    if(k<0)
+        sort(permutation.begin(),permutation.end());
+    else{
+        for( l=n-1;l>=0;l-- )
+        {
+            if(permutation[l]>permutation[k])
+            {
+                swap(permutation[l],permutation[k]);
+                break;
+            }
+        }
+        sort(permutation.begin()+k+1,permutation.end());
+    }
+    return permutation;
+}
